f8523a9a2578f4b6f7d0d74fb9c50cdfe69f7bc9
Maya Rahman <jahant.tech@gmail.com>
2026-08-13 10:30:11 +0600

    up

---
+++ Module/01_my_module.py
@@ -0,0 +1,12 @@
+#function k call korle bracket dite hoy
+#module er modde varaible/function thakte pare ja onno jayga theke call korte pari
+
+'''import fmodule as fm           #fmodule file theke fmodule k call koreci
+                                   #full module k import kora hoise ti dict kaj korse         
+fm.greeting("Jeffy")
+print(fm.person1["age"])'''       #fmodule er dict Person_1 theke age k call
+
+from fmodule import details as d       #fmodule theke preson 1 k call korsi
+print(d["age"])
+
+# greeting("Maya")  #function call kaj korbe na karon puro module k call kora hoy ni
---
+++ Module/fmodule.py
@@ -0,0 +1,19 @@
+'''In Python, Modules are simply files with the “.py”  
+extension containing Python code that can be imported 
+inside another Python Program.'''
+
+#Create a Module
+'''To create a module just save the code 
+you want in a file with the file extension .py: '''
+
+def greeting(name):
+    print("Hello, " + name)
+# greeting("Python")
+
+#---------dictionary nice 
+details = {
+  "name": "Python",
+  "age": 36,
+  "country": "Norway"
+}
+
