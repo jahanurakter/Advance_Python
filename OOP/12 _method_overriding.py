e0238d3c8c33ea498fc751caadc2ea2ec5b18f3f
Maya Rahman <jahant.tech@gmail.com>
2026-08-18 11:14:09 +0600

    up

---
+++ OOP/12 _method_overriding.py
@@ -0,0 +1,25 @@
+#2ta class modde same jinish thakay overriding hocce
+
+# Defining parent class
+class Parent():
+    def ABC(self):
+        # self.value = "Inside Parent"
+        print("This is ABC")
+                                    #aki class er modde 2ta method er modde update ta nile seta overloading
+    # Parent's show method
+    def ABC(self):
+        print("This is ?")
+
+# Defining child class
+class Child(Parent):
+
+    # Constructor
+    def XYZ(self):
+        # self.value = "Inside Child"
+        print("This is ami")
+
+obj1 = Parent()
+obj2 = Child()
+
+obj1.ABC()
+obj2.XYZ()
