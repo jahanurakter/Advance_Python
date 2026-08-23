d4ae28150b856c33f8511b1886f178ab9ac3f20e
Maya Rahman <jahant.tech@gmail.com>
2026-08-23 13:24:54 +0600

    up

---
+++ Advance Problem/01_problem.py
@@ -0,0 +1,25 @@
+# Question: Inventory Management Application
+# Example Run:
+# INVENTORY MANAGEMENT
+# ==============================
+# 1. Add Product
+# 2. View Products
+# 3. Search Product
+# 4. Inventory Value
+# 5. Delete Product
+# 6. Exit
+
+product_list = []
+def add_product():
+    p_id=int(input("Enter Product Id:"))
+    p_name=input("Enter Product Name:")
+    p_price=int(input("Enter Price:"))
+    p_quantity=int(input("Enter Quantity:"))
+    p_data={
+        "p_id": p_id,
+        "p_name": p_name,
+        "p_price": p_price,
+        "p_quantity":p_quantity
+    }
+    product_list.append(p_data)
+add_product()
